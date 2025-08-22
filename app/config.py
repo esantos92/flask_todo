98d0999c45ac67d01f76b98f4619b4cc037044a2
esantos92 <e.santos081992@gmail.com>
2025-08-22 20:04:37 -0300

    feat: start project!

---
+++ app/config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+class Config:
+    SECRET_KEY = getenv('SECRET_KEY', 'dev')
+    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_pre_ping': True}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = getenv('JWT_SECRET_KEY', 'jwt-dev')
+    REDIS_URL = getenv('REDIS_URL', 'redis://localhost:6379/0')
+    CELERY_BROKER_URL = getenv('CELERY_BROKER_URL', REDIS_URL)
+    CELERY_RESULT_BACKEND = getenv('CELERY_RESULT_BACKEND', REDIS_URL)
+
+    APP_PRUNE_DAYS = int(getenv('APP_PRUNE_DAYS', 30))
+
+class Dev(Config): ... # Os três pontos (...) faz a mesma função do pass
+class Prod(Config): ...
